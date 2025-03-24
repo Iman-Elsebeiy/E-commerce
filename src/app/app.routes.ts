6bd0d0d92eb88de84f08fc33ab3375e84c403fd6
iman <iman@127.0.1.1iman-pc.localiman-pc>
2025-03-24 21:25:02 +0200

    bfore json file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-export const routes: Routes = [];
+// Define the routes for the application
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent,
+    title: 'Product List page'
+  },
+{
+  path: 'product/add',
+  component: ProductAddComponent,
+  title: 'Add Product page'
+},
+  {
+    path: 'product/details',
+    component: ProductDetailsComponent,
+    title: 'Product Details page'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: '404 Page Not Found page'
+  }
+];
